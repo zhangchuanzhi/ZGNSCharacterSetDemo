f2b5cdc98ab696619d4cc0b3b49859935929fc35
zhangzhi0903 <18310127735@163.com>
2017-10-16 10:53:28 +0800

    NSCharacterSet字符串筛选

--- ZGNSCharacterSetDemo/ViewController.m
+++ ZGNSCharacterSetDemo/ViewController.m
@@ -8,21 +8,37 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITextFieldDelegate>
+@property(nonatomic,strong)NSString*textField;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    ///去除字符串中所有的@"今"、@"我"、@"s"
+    NSString *str=@"今天阿拉丁减肥了我们sdfkjaldfkla";
+    NSCharacterSet *charSet=[NSCharacterSet characterSetWithCharactersInString:@"今我s"];
+    NSArray *setArr=[str componentsSeparatedByCharactersInSet:charSet];
+    NSString *resultStr1=[setArr componentsJoinedByString:@""];
+     NSLog(@"拆分后的字符串数组------%@\n最终字符串------%@",setArr,resultStr1);
+}
+//**textFielf只能输入数字**
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSCharacterSet *charSet = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+    NSString *filteredStr = [[string componentsSeparatedByCharactersInSet:charSet] componentsJoinedByString:@""];
+    if ([string isEqualToString:filteredStr]) {
+        return YES;
+    }
+    return NO;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 
